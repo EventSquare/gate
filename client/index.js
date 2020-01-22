afa031d434c7f937b4d334007797b7d26cfe0a1d
Willem Staels <willem@redwoodpines.com>
2020-01-22 11:49:33 +0100

    Update client

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 const io = require('socket.io-client');
+const bonjour = require('bonjour');
 const ip = require('ip');
 
 class Client {
@@ -110,7 +111,45 @@ class Client {
             this.events[event](payload);
         }
     }
+    static discover(timeout = 2500,callback){
+        //Initialize Bonjour listener
+        let bonjourBrowser = bonjour();
+        let gates = [];
+        let browser = bonjourBrowser.find({
+            type: 'EventSquare'
+        },function(service){
+            let gate = formatService(service);
+            if(gate) gates.push(gate);
+        });
+        let time = setTimeout(function(){
+            callback(gates);
+            browser.stop();
+        }.bind(this),timeout)
+    }
 }
+
+function formatService(service){
+    //Validate IPV4
+    var ip = findIpv4(service.addresses);
+    if(!ip) return;
+    return {
+        name: service.name,
+        host: ip,
+        port: service.port
+    };
+}
+
+function findIpv4(addresses){
+    let ip = null;
+    for(var i = 0; i < addresses.length; i++){
+        if(addresses[i].length <= 15){
+            ip = addresses[i];
+            break;
+        }
+    }
+    return ip;
+}
+
 
 module.exports = Client;
 
